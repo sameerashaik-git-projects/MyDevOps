ba61e4db89bc57f46873652d901e36e80402bbd6
sameera543 <sameerashk7@gmail.com>
2022-01-30 20:15:24 +0530

    add it

---
+++ users.sh
@@ -0,0 +1,14 @@
+so remember the steps
+
+After installing gitbash:
+
+open it and check
+
+git --version   ---> it shuld display the versio
+cd ~/Desktop   --> go to this directory
+create a directory mkdir <anyfile>
+git init   --> go the director you have created and give this command
+then again go to the director you have created
+	create files
+
+
