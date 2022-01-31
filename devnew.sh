3a5881df542d98ffc3962814152f0406ff969ef9
sameera543 <sameerashk7@gmail.com>
2022-01-31 17:16:19 +0530

    new branch file 2

---
+++ devnew.sh
@@ -0,0 +1,5 @@
+ the trainer said that after creating or modifying a file in other branch  ...if we switch without commiting then the updates will be shown in the branch we switched to
+
+
+
+ so if you switch after commiting then the uodations wo0nt be shown
