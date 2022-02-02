d6a1090121255170438b83dc033c5b53fe5b13a0
sameera543 <sameerashk7@gmail.com>
2022-02-02 17:19:42 +0530

    clonedemo

---
+++ clone.sh
@@ -0,0 +1,6 @@
+this pullls the entire projects(the files,code) and all that are present in the project or repository
+
+
+ So we use clone
+ Before cloning we have to create a directory so that we can clone all the files in to that director then just clone it 
+ git clone <remote url>
