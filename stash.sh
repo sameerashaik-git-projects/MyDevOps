8429f65fc1fececc479dc2d97f3ddba393e333eb
sameera543 <sameerashk7@gmail.com>
2022-02-02 12:23:17 +0530

    -----reverted----- Revert "stash message"
    This reverts commit c89cc48fdceab2692c8da5a1176e3e716e67a7d3.

--- stash.sh
+++
@@ -1,5 +0,0 @@
- This is the stash demo
- He said that the new files cant be taken as backup and are not included in the stash...currently it is in working area lets see if it will be included in the stash..
-   When the new file is in working are it is not included in stash
-
-   I will move to staging area and see if it will be included in stash
--- stash2.sh
+++
@@ -1,1 +0,0 @@
- to remove the confusion between the statsh we are gonna commit it and see the stash
