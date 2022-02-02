a4ed5be7811fa6fe09f2e093482024c93c8debf1
sameera543 <sameerashk7@gmail.com>
2022-02-02 17:40:24 +0530

    pullandfetch

---
+++ pullnfetch.sh
@@ -0,0 +1,12 @@
+The difference between pull and fetch
+
+Fetch directly wont fetch the files into working area
+pull directly fetches files or code to working area
+so after ftech command
+git fetch origin <branch name>
+use git merge orig/branchname 
+
+
+Default aliasname is origin
+
+pull command git pull aliasname branchname
