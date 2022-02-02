5894cd36da0c638202299f4efb9b6d9baeef9efb
sameera543 <sameerashk7@gmail.com>
2022-02-02 13:16:50 +0530

    cherry demo

---
+++ cherry.sh
@@ -0,0 +1,4 @@
+ We are in development branch now... So we want to merge a files in particular commit id
+
+ Then we switch to the branch where we wanna copy these files in commit id
+then give git cherry-pick <commitid>
