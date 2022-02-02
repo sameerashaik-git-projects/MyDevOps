a0a735fb1f441f9b5407d4d493ad6bfc56a5aa9d
sameera543 <sameerashk7@gmail.com>
2022-02-02 12:28:36 +0530

    stash file from commit

---
+++ stash.sh
@@ -0,0 +1,4 @@
+When we do the stash to the newly created files in working area..Those files aare not included in stash
+Now we will add the files from staging area and after the commit and lets see if the files are included in the stash
+from staging are the new file is included in the stash
+Now lets try with the file after the commit
